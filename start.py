e280d6e132a4bd6cf783794d37e835befd2b9e19
Aitejiu <73458598+Aitejiu@users.noreply.github.com>
2024-01-12 15:00:14 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,10 @@
 from openxlab.model import download
 download(model_repo='aitejiu/personal_assistant', output='/home/xlab-app-center/personal_assistant_hf')
-download(model_repo='OpenLMLab/InternLM-7b', output='/root/xlab-app-center/internlm-chat-7b')
+download(model_repo='OpenLMLab/InternLM-7b', output='/home/xlab-app-center/internlm-chat-7b')
 
 import os
 
+os.system("cd /home/xlab-app-center")
 os.system("git clone -b v0.1.9  https://github.com/InternLM/xtuner")
 os.system("cd xtuner")
 os.system("pip install -e '.[all]'")
@@ -12,7 +13,7 @@ os.system("export MKL_SERVICE_FORCE_INTEL=1")
 os.system("export MKL_THREADING_LAYER='GNU'")
 os.system("mkdir merged")
 os.system("xtuner convert merge \
-    /root/xlab-app-center/internlm-chat-7b \
+    /home/xlab-app-center/internlm-chat-7b \
     /home/xlab-app-center/personal_assistant_hf \
     /home/xlab-app-center/merged \
     --max-shard-size 2GB")
